9dfec1ec99f03c7540332cfa3d9a5fa8d6017374
jyotishankar04 <patrajyotishankar@gmail.com>
2025-03-01 12:12:56 +0530

    v1:fix:frontend

--- frontend/src/lib/axios/axiosApis.tsx
+++ frontend/src/lib/axios/axiosApis.tsx
@@ -12,7 +12,10 @@ export const axiosApi = axios.create({
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
+    "Access-Control-Allow-Credentials": "true",
   },
+  xsrfCookieName: "XSRF-TOKEN",
+  xsrfHeaderName: "X-XSRF-TOKEN",
 });
 
 // Auth API
